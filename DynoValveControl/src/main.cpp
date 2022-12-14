2e6a7f3302cfaa4ddb996e502cc3a393070e539c
BenLeistiko <ben.leistiko@gmail.com>
2022-12-13 19:38:26 -0800

    Implement basic tasks, RPM HF rejection, and continuous averaging

--- DynoValveControl/src/main.cpp
+++ DynoValveControl/src/main.cpp
@@ -6,30 +6,117 @@
 HX711 lc;
 float val = 0;
 
-volatile byte rpmcount=0;
+volatile byte rpmcount = 0;
 
-unsigned int rpm=0;
-unsigned long timeold=0;
+unsigned int engineSpeed = 0;                                           // RPM
+unsigned long lastTime = 0;                                             // ms
+const int triggersPerRot = 4;                                           // -
+int HFRejectionRPM = 4500;                                              // RPM
+unsigned long DeltaTime = 60000000 / (HFRejectionRPM * triggersPerRot); // ms
+unsigned long SpeedHistory[triggersPerRot * 2] = {0};
+bool updateEngineSpeed = 0;
+int steps = 200;       // Generic stepper motor
+int speed = steps * 1; // add conversion "something to give me speed"
 
-void myInterrupt(){
-    rpmcount++;
-    if(rpmcount >=8){
-        unsigned int curtime = millis();
-        rpm = ((rpmcount/8)*2*60000)/(curtime-timeold);
-        timeold = curtime;
+void engineHallISR()
+{
+    unsigned int currTime = micros();
+    if (currTime - lastTime > DeltaTime)
+    {
+        rpmcount++;
+        lastTime = currTime;
+        SpeedHistory[rpmcount] = engineSpeed;
+    }
+    if (rpmcount > 7)
+    {
         rpmcount = 0;
     }
+    updateEngineSpeed = 1;
 }
 
-void setup() {
+void controllerISR()
+{
+}
+
+void setup()
+{
     lc.begin(12, 11);
     lc.set_offset(0);
     Serial.begin(115200);
     pinMode(0, INPUT);
-    attachInterrupt(digitalPinToInterrupt(0), myInterrupt, RISING);
+    attachInterrupt(digitalPinToInterrupt(0), engineHallISR, RISING);
 }
 
-void loop() {
-        val = float(lc.read()) / 9852-3.5;
-        Serial.print(millis());Serial.print(",");Serial.print(val);Serial.print(","); Serial.println(rpm);
+void loop()
+{
+    updateEngine();
+    updateLC();
+    updateStepper();
+    updateData();
+}
+
+void updateEngine()
+{
+    if (updateEngineSpeed)
+    {
+        engineSpeed = 120000000 / (SpeedHistory[rpmcount] - SpeedHistory[(rpmcount + 1) % (2 * triggersPerRot + 1)]);
+    }
+    else if (micros() - lastTime > 500000)
+    {
+        engineSpeed = 0;
+    }
+}
+
+void updateLC()
+{
+    if (lc.is_ready())
+    {
+        val = float(lc.read()) / 9852 - 3.5;
+    }
+}
+
+unsigned long lastWriteTime = 0;
+
+void updateData()
+{
+    unsigned int currTime = micros();
+    if (currTime - lastWriteTime > 10000)
+    {
+        lastWriteTime = currTime;
+        Serial.print(millis());
+        Serial.print(",");
+        Serial.print(val);
+        Serial.print(",");
+        Serial.println(engineSpeed);
+    }
+}
+
+enum StepperState
+{
+    INIT,
+    ZERO_RETURN,
+    REST,
+    CONTROL
+};
+StepperState stepperState = INIT;
+
+int curPos = 0; // Steps
+
+void updateStepper()
+{
+
+    switch (stepperState)
+    {
+    case INIT:
+
+    case ZERO_RETURN:
+
+    case REST:
+
+    case CONTROL:
+    }
+}
+
+void step(int speed)
+{
 }
