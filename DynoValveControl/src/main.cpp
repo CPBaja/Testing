2903b0d86978a327b520a4774362b43661fb8389
BenLeistiko <ben.leistiko@gmail.com>
2022-12-15 18:15:59 -0800

    Update engine sensing to store the correct values

--- DynoValveControl/src/main.cpp
+++ DynoValveControl/src/main.cpp
@@ -9,7 +9,7 @@
 #define MS2 14
 #define MS3 15
 #define EN 39
-#define LS 00 // Pin for limit switch
+#define LS 32 // Pin for limit switch
 
 #define LC1 12 // Pin for limit switch
 #define LC2 11 // Pin for limit switch
@@ -29,42 +29,57 @@ unsigned long lastEngineTime = 0;                                       // us
 const int triggersPerRot = 4;                                           // -
 int HFRejectionRPM = 4500;                                              // RPM
 unsigned long DeltaTime = 60000000 / (HFRejectionRPM * triggersPerRot); // us
-unsigned long SpeedHistory[triggersPerRot * 2] = {0};
+unsigned long SpeedHistory[triggersPerRot * 2 + 1] = {0};
 bool updateEngineSpeed = 0;
 int steps = 200;             // Generic stepper motor
 int stepangle = 360 / steps; // step angle
 
-int minEngineSpeed = 2400;                                                // RPM
-int maxEngineSpeed = 3600;                                                // RPM
-int enginePeriod = 4 * 60 * 1000 * 1000;                                  // min*60*1000*1000 = us
-int engineSlope = (enginePeriod / 2) / (maxEngineSpeed - minEngineSpeed); // delta us / engine speed
-int currEngineTarget = maxEngineSpeed;                                    // RPM
-bool speedUpEngine = 0;                                                   // 1 = increase engine speed, 0 = slow down engine speed
+int minEngineSpeed = 2400;                                                          // RPM
+int maxEngineSpeed = 3600;                                                          // RPM
+unsigned long enginePeriod = 4 * 60 * 1000 * 1000;                                  // min*60*1000*1000 = us
+unsigned long engineSlope = (enginePeriod / 2) / (maxEngineSpeed - minEngineSpeed); // delta us / engine speed
+int engineTarget = maxEngineSpeed;                                                  // RPM
+bool speedUpEngine = 0;                                                             // 1 = increase engine speed, 0 = slow down engine speed
+unsigned long lastMaxMinEngineTime = 0;                                             // last time the target engine speed was at a min or max value
 
 bool engineOn = 0;
-int engineKP = 1;
-int engineKI = 0.1;
+float engineKP = 1;
+float engineKI = 0.1;
 int engineIntegral = 0;
 
 int valveTargetPos = 0; // Target valve pos
 
+// 17HS15-1684S-PG5
+// 5.18 : 1 gearbox
+// 200 steps per motor rotation
+// 1036 steps per output rotation
+// ~200 RPM max
+// ~8.5 rotations full open -> full close
+// zero 1.5 rotations from open
+// ~7 rotations zero -> full close
+// 0.5 rotation from zero = rest position, 500 steps
+// 1 rotations from zero = min position, 1000 steps
+// 6 rotations from zero = max position, 6000 steps
+// 0.5 from close = safety ~6.5*1036 ~ 6700 rotations absolute max
+
 int valveMinPos = 0;       // Fully open valve
 int valveMaxPos = 1000;    // Fully closed valve
-int minPosSteps = 1000;    // Number of steps at open valve
-int maxPosSteps = 2000;    // Number of steps at closed valve
-int absMaxPosSteps = 2500; // Checked for saftey when attempting to acutate stepper, won't step if curStep >=  absMaxPosSteps
+int minPosSteps = 500;     // Number of steps at open valve
+int maxPosSteps = 6000;    // Number of steps at closed valve
+int absMaxPosSteps = 6700; // Checked for saftey when attempting to acutate stepper, won't step if curStep >=  absMaxPosSteps
+int restPostion = 1000;    // Move away from the zero postion at rest by this much
 
 int targetSteps = 0; // target postion in steps, ie scaled valveTargetPos
 int curSteps = 0;    // Count the number of steps (current position)
 
 bool stepperOn = 0; // Turn on and off stepper controller
-int stepperKP = 1;
-int stepperKI = 0.1;
+float stepperKP = 1;
+float stepperKI = 0.1;
 int stepperIntegral = 0;
 int stepperSpeed = 0;      // how fast to move the stepper
-int maxStepperSpeed = 100; // RPM max stepper speed
+int maxStepperSpeed = 200; // RPM max stepper speed
 
-int restPostion = 100; // Move away from the zero postion at rest by this much
+unsigned long lastDataWriteTime = 0;
 
 enum StepperState
 {
@@ -104,69 +119,69 @@ void setup()
     pinMode(LS, INPUT_PULLUP); // setup limit switch
 
     attachInterrupt(digitalPinToInterrupt(HS), engineHallISR, RISING); // Attach engine speed interupt to the hall effect sensor
-    ContorllerTimer.begin(ControllerISR, 5000);                        // Run control loop every 5 ms
+    // ContorllerTimer.begin(ControllerISR, 5000);                        // Run control loop every 5 ms
 }
 
 void loop()
 {
     updateEngine();
-    updateLC();
-    updateStepper();
+    // updateLC();
+    // updateStepper();
     updateData();
 }
 
 void engineHallISR()
 {
-    unsigned int currTime = micros();
+    unsigned long currTime = micros();
     if (currTime - lastEngineTime > DeltaTime)
     {
-        rpmcount++;
         lastEngineTime = currTime;
-        SpeedHistory[rpmcount] = engineSpeed;
+        SpeedHistory[rpmcount] = currTime;
+        rpmcount++;
     }
-    if (rpmcount > 7)
+    if (rpmcount > 8)
     {
         rpmcount = 0;
     }
     updateEngineSpeed = 1;
 }
-/*
-int minEngineSpeed = 2400;             // RPM
-int maxEngineSpeed = 3600;             // RPM
-int period = 4 * 60 * 1000 * 1000;     // MIN*60*1000*1000
-int currEngineTarget = maxEngineSpeed; // RPM
-bool speedUpEngine = 0;                // 1 = increase engine speed, 0 = slow down engine speed
 
-bool engineOn = 0;
-int engineKP = 1;
-int engineKI = 0.1;
-int engineIntegral = 0;
-
-int valveTargetPos = 0; // Target valve pos
-
-int valveMinPos = 0;       // Fully open valve
-int valveMaxPos = 1000;    // Fully closed valve
-int minPosSteps = 1000;    // Number of steps at open valve
-int maxPosSteps = 2000;    // Number of steps at closed valve
-int absMaxPosSteps = 2500; // Checked for saftey when attempting to acutate stepper, won't step if curStep >=  absMaxPosSteps
-
-int targetSteps = 0; // target postion in steps, ie scaled valveTargetPos
-int curSteps = 0;    // Count the number of steps (current position)
-
-bool stepperOn = 0; // Turn on and off stepper controller
-int stepperKP = 1;
-int stepperKI = 0.1;
-int stepperIntegral = 0;
-int stepperSpeed = 0;      // how fast to move the stepper
-int maxStepperSpeed = 100; // RPM max stepper speed
-*/
 void ControllerISR()
 {
+    unsigned long currTime = micros();
+    if (speedUpEngine)
+    {
+        engineTarget = (currTime - lastMaxMinEngineTime) / engineSlope + minEngineSpeed;
+    }
+    else
+    {
+        engineTarget = maxEngineSpeed - (currTime - lastMaxMinEngineTime) / engineSlope;
+    }
+
+    if (engineTarget > maxEngineSpeed || engineTarget < minEngineSpeed)
+    {
+        speedUpEngine = !speedUpEngine;
+        lastMaxMinEngineTime = micros();
+    }
+
     if (engineOn)
     {
+        int engineError = engineTarget - engineSpeed;
+        if (valveTargetPos < valveMaxPos && valveTargetPos > valveMinPos)
+        {
+            engineIntegral += engineError;
+        }
+        valveTargetPos = engineKP * engineError + engineKI * engineIntegral;
+        targetSteps = minPosSteps + (maxPosSteps - minPosSteps) * (valveTargetPos - valveMinPos) / (valveMaxPos - valveMinPos);
     }
     if (stepperOn)
     {
+        int stepperError = targetSteps - curSteps;
+        if (abs(stepperSpeed) < maxStepperSpeed)
+        {
+            stepperIntegral += stepperError;
+        }
+        stepperSpeed = stepperKP * stepperError + stepperKI * stepperIntegral;
     }
 }
 
@@ -190,14 +205,12 @@ void updateLC()
     }
 }
 
-unsigned long lastWriteTime = 0;
-
 void updateData()
 {
     unsigned int currTime = micros();
-    if (currTime - lastWriteTime > 10000)
+    if (currTime - lastDataWriteTime > 10000)
     {
-        lastWriteTime = currTime;
+        lastDataWriteTime = currTime;
         Serial.print(millis());
         Serial.print(",");
         Serial.print(val);
@@ -222,6 +235,7 @@ void updateStepper()
             targetSteps = restPostion;
             stepperOn = true;
             stepperState = REST;
+            curSteps = 0;
             sei();
         }
         else
@@ -233,7 +247,8 @@ void updateStepper()
         step(stepperSpeed);
         if (engineSpeed > 3500 && abs(curSteps - restPostion) < 0.05 * restPostion)
         {
-            currEngineTarget = maxEngineSpeed;
+            engineTarget = maxEngineSpeed;
+            lastMaxMinEngineTime = micros();
             engineOn = true;
             stepperState = CONTROL;
         }
@@ -277,6 +292,14 @@ void step(int speed) // Input Speed in RPM, saturate to max speed (delay of 1 ms
             STP = HIGH;
             digitalWrite(STP, HIGH);
             lastStepTime = currTime;
+            if (speed > 0)
+            {
+                curSteps += 1;
+            }
+            else
+            {
+                curSteps -= 1;
+            }
         }
         else if (STP && currTime - lastStepTime > 20)
         {
@@ -299,5 +322,6 @@ void setupBEDPins()
     digitalWrite(dir, LOW);
     digitalWrite(MS1, LOW);
     digitalWrite(MS2, LOW);
+    digitalWrite(MS3, LOW);
     digitalWrite(EN, HIGH);
 }
